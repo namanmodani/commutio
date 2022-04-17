cfafdff4667e086bc9d0159718221d6d78f136ec
Naman Modani <modani.naman@gmail.com>
2022-04-16 17:55:16 -0700

    index update, remove stepbutton

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,40 +5,27 @@ import styles from '../styles/Home.module.css'
 import { Button, Col } from "react-bootstrap";
 import { fontGrid } from '@mui/material/styles/cssUtils';
 import styled from 'styled-components';
-import StepButton from '../components/step-button';
 import GeneralButton from '../components/button';
 import { stepButtonClasses } from '@mui/material';
 
-import { addUser, checkLogin } from '../firebase';
-
-// addUser("shlokj@g.ucla.edu","password");
-checkLogin("shlokj@g.ucla.edu","password");
-
 const Home: NextPage = () => {
-  return (
-      <div>
-          <div >
-              <h1 style = {{
-                  paddingTop: "",
-                  fontSize: "",
-                  color: "",
-                  fontWeight: "",
-                  fontFamily: '',
-              }}>
-              </h1>
-
-              <Col>
-                  <div>
-                      <GeneralButton name = "Sign In" href = '/login'/>
-                  </div>
-                  <div>
-                      <GeneralButton name = "Sign Up" href = '/signup'/>
-                  </div>
-              </Col>
-
-          </div>
-      </div>
-  )
+    return (
+        <div>
+            <div>
+                <div>
+                    <img id = "logo" src = 'commutio.png' alt = 'commutio logo' width = {120} height = {130} style = {{paddingTop: "200px",}}/>
+                </div>
+                <Col>
+                    <div>
+                        <GeneralButton name = "Sign In" href = '/login'/>
+                    </div>
+                    <div>
+                        <GeneralButton name = "Sign Up" href = '/signup'/>
+                    </div>
+                </Col>
+            </div>
+        </div>
+    )
 }
 
 export default Home;
